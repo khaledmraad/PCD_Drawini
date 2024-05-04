90bb38141343b6151e709c829636e216ac16e52f
jassercherif <jasser.cherif@ensi-uma.tn>
2024-05-04 12:14:59 +0100

    Adding Requirements

--- frontend/src/Component/MainCanvas/component/Main.jsx
+++ frontend/src/Component/MainCanvas/component/Main.jsx
@@ -199,9 +199,10 @@ export default function Main() {
                 <select id="countries"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
-                    <option value="US">Angular</option>
-                    <option value="CA">React</option>
-                    <option value="FR">HTML/CSS</option>
+                    <option value="html">HTML/CSS</option>
+                    <option value="angular">Angular</option>
+                    <option value="react_js">React Js</option>
+                    <option value="react_ts">React Ts</option>
                 </select>
             </div>
 
