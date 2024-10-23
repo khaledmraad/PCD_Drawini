69455819be4e218b335d7a223a4167e6e40e6c58
khaledmraad <khaled.mrad@ensi-uma.tn>
2024-10-23 20:20:55 +0100

    for real kubernetes?? yes

--- Frontend_V2.0/src/components/Signup/index.tsx
+++ Frontend_V2.0/src/components/Signup/index.tsx
@@ -35,7 +35,7 @@ export default function Signup(){
     async function btnSignup(event: React.FormEvent<HTMLFormElement>): Promise<boolean> {
       event.preventDefault(); 
     
-      const url = 'http://4.180.6.234:8080/api/v1/auth/register';
+      const url = 'https://drawini.local.com/api/v1/auth/register';
     
       const data = loginForm;
     
