5f5658b90c35eee6660dd6ebd62b6f0966facdb5
khaledmraad <khaled.mrad@ensi-uma.tn>
2025-01-11 18:40:52 +0000

    better terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,171 @@
+# main.tf
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.0"
+#     }
+#     tls = {
+#       source  = "hashicorp/tls"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
+
 provider "azurerm" {
-  features {}
   subscription_id = "62e994c5-446e-45c1-b283-d310c051240f"
-
+  features {}
 }
 
-resource "azurerm_resource_group" "example_group" {
-  name     = "aks_is_shit"
-  location = "italynorth"
-
+# Generate SSH key pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
+# Save private key to file
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "aks-ssh-key.pem"
+  file_permission = "0600"
+}
 
+resource "azurerm_resource_group" "aks" {
+  name     = "aks-resource-group"
+  location = "eastus"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "aks-vnet"
+  address_space       = ["10.224.0.0/16"]
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+}
+
+# AKS Subnet
+resource "azurerm_subnet" "aks" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.aks.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.224.0.0/24"]
+}
+
+# Jump Box Subnet
+resource "azurerm_subnet" "jump" {
+  name                 = "jump-subnet"
+  resource_group_name  = azurerm_resource_group.aks.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.224.1.0/24"]
+}
+
+# Public IP for Jump Box
+resource "azurerm_public_ip" "jump" {
+  name                = "jump-ip"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  allocation_method   = "Static"
+  sku                = "Standard"
+}
+
+# NSG for Jump Box
+resource "azurerm_network_security_group" "jump" {
+  name                = "jump-nsg"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range         = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Jump Box NIC
+resource "azurerm_network_interface" "jump" {
+  name                = "jump-nic"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.jump.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.jump.id
+  }
+}
+
+# Jump Box VM
+resource "azurerm_linux_virtual_machine" "jump" {
+  name                = "jump-vm"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.jump.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.jump.id
+  network_security_group_id = azurerm_network_security_group.jump.id
+}
+
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "op_cluster"
-  location            = "italynorth"
-  resource_group_name = "aks_is_shit"
-  dns_prefix = "yes-dns"
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "myakscluster"
 
   default_node_pool {
-    name            = "testnodepool"
-    node_count      = 1
-    vm_size         = "Standard_DS2_v2"
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.aks.id
   }
 
-  identity { #how aks access to azure recources
-      type = "SystemAssigned"
+  linux_profile {
+    admin_username = "azureuser"
+    ssh_key {
+      key_data = tls_private_key.ssh_key.public_key_openssh
+    }
   }
 
-
-  tags = {
-    environment = "dev"
+  identity {
+    type = "SystemAssigned"
   }
 
+  network_profile {
+    network_plugin     = "kubenet"
+    load_balancer_sku = "standard"
+  }
+}
+
+output "jump_box_public_ip" {
+  value = azurerm_public_ip.jump.ip_address
 }
